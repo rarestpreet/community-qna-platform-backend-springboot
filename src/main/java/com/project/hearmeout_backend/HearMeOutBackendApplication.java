79a5be41a33abf7d4e99d55ea560f73ab54c540f
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-03-10 22:49:44 +0530

    started on 10/03/26

---
+++ src/main/java/com/project/hearmeout_backend/HearMeOutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.hearmeout_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearMeOutBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearMeOutBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hearmeout_backend/HearMeOutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hearmeout_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearMeOutBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
