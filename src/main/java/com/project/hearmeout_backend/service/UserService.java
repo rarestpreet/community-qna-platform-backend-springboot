a301c4d516f8f18f286f74874b1d5eb68d257d70
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-03-27 03:48:30 +0530

    refractor: minor business logic changes

--- src/main/java/com/project/hearmeout_backend/service/UserService.java
+++ src/main/java/com/project/hearmeout_backend/service/UserService.java
@@ -1,17 +1,15 @@
 package com.project.hearmeout_backend.service;
 
-import com.project.hearmeout_backend.dto.request.SecurityRequest.RegisterRequestDTO;
-import com.project.hearmeout_backend.dto.request.UserRequest.UserProfileRequestDTO;
-import com.project.hearmeout_backend.dto.response.UserResponse.UserAnswerResponseDTO;
-import com.project.hearmeout_backend.dto.response.UserResponse.UserCommentResponseDTO;
-import com.project.hearmeout_backend.dto.response.UserResponse.UserProfileResponseDTO;
-import com.project.hearmeout_backend.dto.response.UserResponse.UserQuestionResponseDTO;
+import com.project.hearmeout_backend.dto.request.user_request.UserProfileRequestDTO;
+import com.project.hearmeout_backend.dto.response.user_response.UserAnswerResponseDTO;
+import com.project.hearmeout_backend.dto.response.user_response.UserCommentResponseDTO;
+import com.project.hearmeout_backend.dto.response.user_response.UserProfileResponseDTO;
+import com.project.hearmeout_backend.dto.response.user_response.UserQuestionResponseDTO;
 import com.project.hearmeout_backend.exception.EmailAlreadyExistException;
 import com.project.hearmeout_backend.exception.UserNotFoundException;
 import com.project.hearmeout_backend.exception.UsernameAlreadyExistException;
-import com.project.hearmeout_backend.mapper.AnswerMapper;
 import com.project.hearmeout_backend.mapper.CommentMapper;
-import com.project.hearmeout_backend.mapper.QuestionMapper;
+import com.project.hearmeout_backend.mapper.PostMapper;
 import com.project.hearmeout_backend.mapper.UserMapper;
 import com.project.hearmeout_backend.model.Comment;
 import com.project.hearmeout_backend.model.Post;
@@ -20,10 +18,12 @@ import com.project.hearmeout_backend.model.enums.PostType;
 import com.project.hearmeout_backend.repository.CommentRepository;
 import com.project.hearmeout_backend.repository.PostRepository;
 import com.project.hearmeout_backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -44,15 +44,20 @@ public class UserService {
         List<Post> questions = postRepo.findByAuthor_IdAndPostType(currUser.getId(), PostType.QUESTION);
 
         return questions.stream()
-                .map(QuestionMapper::toQuestionDTO)
+                .map(PostMapper::toUserQuestionResponseDTO)
                 .toList();
     }
 
-    private User checkAndGetUser(String username) throws UserNotFoundException {
+    public User checkAndGetUser(Long userId)
+            throws UserNotFoundException {
+        return userRepo.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
+    }
+
+    public User checkAndGetUser(String username)
+            throws UserNotFoundException {
         return userRepo.findByUsername(username)
-                .orElseThrow(() ->
-                        new UserNotFoundException("User not found with username: " + username)
-                );
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + username));
     }
 
     public List<UserAnswerResponseDTO> getUserAnswers(String username)
@@ -61,7 +66,7 @@ public class UserService {
         List<Post> answers = postRepo.findByAuthor_IdAndPostType(currUser.getId(), PostType.ANSWER);
 
         return answers.stream()
-                .map(AnswerMapper::toAnswerDTO)
+                .map(PostMapper::toUserAnswerResponseDTO)
                 .toList();
     }
 
@@ -71,21 +76,23 @@ public class UserService {
         List<Comment> comments = commentRepo.findAllByAuthor_Id(currUser.getId());
 
         return comments.stream()
-                .map(CommentMapper::toCommentDto)
+                .map(CommentMapper::toUserCommentResponseDto)
                 .toList();
     }
 
-    public void updateUserDetails(String
-                                          username, UserProfileRequestDTO userProfileRequestDTO)
+    @Transactional
+    public void updateUserDetails(String username, UserProfileRequestDTO userProfileRequestDTO)
             throws UserNotFoundException, UsernameAlreadyExistException, EmailAlreadyExistException {
-        if (userRepo.existsByUsername(userProfileRequestDTO.getUsername())) {
+        User currUser = checkAndGetUser(username);
+
+        if (!Objects.equals(currUser.getUsername(), userProfileRequestDTO.getUsername()) &&
+                userRepo.existsByUsername(userProfileRequestDTO.getUsername())) {
             throw new UsernameAlreadyExistException("Username is already taken");
         }
-        if (userRepo.existsByEmail(userProfileRequestDTO.getEmail())) {
+        if (!currUser.getEmail().equals(userProfileRequestDTO.getEmail()) &&
+                userRepo.existsByEmail(userProfileRequestDTO.getEmail())) {
             throw new EmailAlreadyExistException("Email is already registered");
         }
-
-        User currUser = checkAndGetUser(username);
 
         currUser.setUsername(userProfileRequestDTO.getUsername());
         currUser.setEmail(userProfileRequestDTO.getEmail());
@@ -95,17 +102,19 @@ public class UserService {
         userRepo.save(currUser);
     }
 
+    @Transactional
     public void terminateUserAccount(String username)
             throws UserNotFoundException {
         User currUser = checkAndGetUser(username);
 
         currUser.setAccountTerminated(true);
-        currUser.setUsername(null);
-        currUser.setEmail(null);
-        currUser.setPassword(null);
+        currUser.setUsername("DELETED_"+currUser.getUsername());
+        currUser.setEmail("DELETED_"+currUser.getEmail());
+        currUser.setPassword("DELETED_"+currUser.getPassword());
         currUser.setEmailVerified(false);
         currUser.setReputation(0);
 
         userRepo.save(currUser);
     }
+
 }
