321feb82138ed108db68222ba1971d2033e26b91
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-03-26 20:19:32 +0530

    feat(security): add custom user detail

---
+++ src/main/java/com/project/hearmeout_backend/model/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.project.hearmeout_backend.model;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NullMarked
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ src/main/java/com/project/hearmeout_backend/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.project.hearmeout_backend.service;
+
+import com.project.hearmeout_backend.exception.UserNotFoundException;
+import com.project.hearmeout_backend.model.CustomUserDetails;
+import com.project.hearmeout_backend.model.User;
+import com.project.hearmeout_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@NullMarked
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername( String username) throws UsernameNotFoundException {
+        User currUser = userRepo.findByEmail(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found. Enter registered email"));
+
+        return new CustomUserDetails(currUser);
+    }
+}
