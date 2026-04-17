3616f9b029b6645e50b5ac2fdca0c3c5dd5cf887
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-04-17 23:39:41 +0530

    chore: update gitignore, add basic test assertion, remove what_to_do_next.md

--- src/test/java/com/project/hearmeout_backend/HearMeOutBackendApplicationTests.java
+++ src/test/java/com/project/hearmeout_backend/HearMeOutBackendApplicationTests.java
@@ -3,11 +3,14 @@ package com.project.hearmeout_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class HearMeOutBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(2+2, 4);
     }
 
 }
